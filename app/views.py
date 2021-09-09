d06f715a1e1d120a3a6bffe3850ac389943c7de6
hythang2020 <hythang2020@gmail.com>
2021-09-09 21:01:59 +0700

    up

--- app/views.py
+++ app/views.py
@@ -3,8 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello world !!')
+    return render(request, 'home.html')
+    #return HttpResponse('Hello world !!')
 
 
 def product(request):
-    return HttpResponse('Product list view !!!')
+    data = {
+        "products":[{
+            "name": "Tivi 01",
+            "price": "20.000",
+        },
+        {
+            "name": "Tivi 02",
+            "price": "50.000",
+        },{
+            "name": "Tivi 03",
+            "price": "70.000",
+        },
+        ]
+    }
+    return render(request, 'product.html',data)
+    #return HttpResponse('Product list view !!!')
